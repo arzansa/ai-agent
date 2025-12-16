8eff57146e8e5010a9228029da56856e8f8392f1
arzansa <arzansa@gmail.com>
2025-12-15 21:45:23 -0600

    add arg parsing

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
+import argparse
 from dotenv import load_dotenv
 from google import genai
-
 
 def main():
     print("Hello from ai-agent!")
@@ -14,9 +14,11 @@ def main():
 
     client = genai.Client(api_key=api_key)
 
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
 
-    
-    response = client.models.generate_content(model="gemini-2.5-flash", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+    response = client.models.generate_content(model="gemini-2.5-flash", contents=args.user_prompt)
     
     if response.usage_metadata:
         metadata = response.usage_metadata
